f60a6de8e5955bae62ba20182acfa84839baa5ca
Izzat Alsharif <work@izzatalsharif.com>
2024-04-06 02:30:31 +0300

    Add three queries

--- Scripts/QUERIES.sql
+++ Scripts/QUERIES.sql
@@ -101,3 +101,95 @@ SELECT
 FROM Catalog.Genres g
 LEFT JOIN GenreBorrows gb ON g.GenreID = gb.GenreID
 ORDER BY PopularityRank;
+
+
+
+-- 8. Overdue Analysis:
+-- List all books overdue by more than 30 days with their associated borrowers.
+DECLARE @OverdueDays INT = 10;
+WITH OverdueBooks AS (
+    SELECT
+        BookID,
+        BorrowerID,
+		DateBorrowed,
+        DueDate,
+        DateReturned,
+        OverdueDays = DATEDIFF(DAY, DueDate, COALESCE(DateReturned, GETDATE()))
+    FROM Circulation.Loans
+    WHERE DATEDIFF(DAY, DueDate, COALESCE(DateReturned, GETDATE())) > @OverdueDays
+)
+SELECT
+    k.BookID,
+    k.Title,
+    k.Author,
+    w.BorrowerID,
+    w.FirstName,
+    w.LastName,
+    w.Email,
+	o.DateBorrowed,
+    o.DueDate,
+    o.DateReturned,
+    o.OverdueDays
+FROM OverdueBooks o
+JOIN Catalog.Books k ON o.BookID = k.BookID
+JOIN UserManagement.Borrowers w ON o.BorrowerID = w.BorrowerID
+ORDER BY OverdueDays DESC, w.FirstName;
+
+
+
+-- 9. Author Popularity using Aggregation:
+-- Rank authors by the borrowing frequency of their books.
+WITH AuthorFrequency AS (
+    SELECT
+        b.Author,
+        BorrowingCount = COUNT(l.LoanID)
+    FROM Catalog.Books b
+    LEFT JOIN Circulation.Loans l ON b.BookID = l.BookID
+	GROUP BY b.Author
+)
+SELECT
+    Author,
+    BorrowingCount,
+    Popularity = DENSE_RANK() OVER (ORDER BY BorrowingCount DESC)
+FROM AuthorFrequency
+ORDER BY BorrowingCount DESC, Author;
+
+
+
+-- 10. Genre Preference by Age using Group By and Having:
+-- Determine the preferred genre of different age groups of borrowers.
+-- (Groups are (0,10), (11,20), (21,30)…)
+DECLARE @GroupSize INT = 10;
+WITH BorrowerGenreFrequency AS (
+    SELECT
+        l.BorrowerID,
+        bg.GenreID,
+        BorrowingFrequency = COUNT(l.LoanID)
+    FROM Circulation.Loans l
+    JOIN Catalog.BookGenres bg ON l.BookID = bg.BookID
+    GROUP BY l.BorrowerID, bg.GenreID
+),
+BorrowerGroups AS (
+    SELECT
+        BorrowerID,
+        GroupNumber = DATEDIFF(YEAR, DateOfBirth, GETDATE()) / @GroupSize
+    FROM UserManagement.Borrowers
+),
+BorrowerGroupsGenreFrequency AS (
+    SELECT
+        bgf.GenreID,
+        bg.GroupNumber,
+        BorrowingFrequency = SUM(bgf.BorrowingFrequency)
+    FROM BorrowerGenreFrequency bgf
+    JOIN BorrowerGroups bg ON bgf.BorrowerID = bg.BorrowerID
+	GROUP BY bgf.GenreID, bg.GroupNumber
+)
+SELECT
+    g.GenreID,
+    g.Name,
+    BorrowingFrequency = ISNULL(bggf.BorrowingFrequency, 0),
+    GroupAge = CONCAT(bggf.GroupNumber * @GroupSize, ' - ', (bggf.GroupNumber + 1) * @GroupSize - 1),
+    Popularity = DENSE_RANK() OVER (PARTITION BY bggf.GroupNumber ORDER BY ISNULL(bggf.BorrowingFrequency, 0) DESC)
+FROM Catalog.Genres g
+LEFT JOIN BorrowerGroupsGenreFrequency bggf ON g.GenreID = bggf.GenreID
+ORDER BY bggf.GroupNumber, ISNULL(bggf.BorrowingFrequency, 0) DESC;
