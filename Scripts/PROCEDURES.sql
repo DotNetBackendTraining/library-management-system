dab860ae08b4a7969fa2144a59b66471f5af6210
Izzat Alsharif <work@izzatalsharif.com>
2024-04-08 01:57:51 +0300

    Add procedure

--- Scripts/PROCEDURES.sql
+++ Scripts/PROCEDURES.sql
@@ -67,3 +67,42 @@ END
 
 -- Example
 EXEC sp_BorrowedBooksReport @StartDate = '2024-02-01', @EndDate = '2024-03-01';
+
+
+
+-- 13. SQL Stored Procedure with Temp Table:
+-- Retrieves all borrowers who have overdue books. Stores these borrowers in a temporary table,
+-- then joins this temp table with the Loans table to list out the specific overdue books for each borrower.
+CREATE OR ALTER PROCEDURE sp_ListOverdueBooksByBorrower
+AS BEGIN
+    SET NOCOUNT ON;
+
+    IF OBJECT_ID('tempdb..#OverdueBorrowers') IS NOT NULL
+        DROP TABLE #OverdueBorrowers;
+
+    SELECT DISTINCT
+        br.BorrowerID,
+        br.FirstName,
+        br.LastName
+    INTO #OverdueBorrowers
+    FROM UserManagement.Borrowers br
+    JOIN Circulation.Loans l ON br.BorrowerID = l.BorrowerID
+    WHERE l.DueDate < GETDATE() AND (l.DateReturned IS NULL OR l.DateReturned > l.DueDate);
+
+    SELECT
+        ob.BorrowerID,
+        ob.FirstName,
+        ob.LastName,
+        b.Title AS BookTitle,
+        l.DateBorrowed,
+        l.DueDate,
+		l.DateReturned
+    FROM #OverdueBorrowers ob
+    JOIN Circulation.Loans l ON ob.BorrowerID = l.BorrowerID
+    JOIN Catalog.Books b ON l.BookID = b.BookID
+    WHERE l.DueDate < GETDATE() AND (l.DateReturned IS NULL OR l.DateReturned > l.DueDate)
+    ORDER BY ob.BorrowerID, l.DueDate;
+END;
+
+-- Example
+EXEC sp_ListOverdueBooksByBorrower;
